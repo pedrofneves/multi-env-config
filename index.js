05a8afcc0c7dcce16ee41d4696afe1b7a6d80b92
pedrofneves <pedro.f.neves@gmail.com>
2018-02-15 23:23:34 -0200

    alterando abordagem para iniciar o módulo

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 let environmentName = 'NODE_ENV';
 let configDirectory = './src/Config/';
 
-module.exports = function(_environmentName, _configDirectory){
+module.exports = {
 
-	const path = require('path');
-	
-	if(_environmentName != undefined){
-		environmentName = _environmentName;
+	init: function(_environmentName, _configDirectory){
+
+		if(_environmentName != undefined){
+			environmentName = _environmentName;
+		}
+		console.log('Config environment: ' +  process.env[environmentName]);
+		
+		if(_configDirectory != undefined){
+			configDirectory = _configDirectory;
+		}
+		console.log('Config configDirectory: ' + configDirectory);
+			
+		console.log('Config environment: ' +  process.env[environmentName]);
+		console.log('Config configDirectory: ' + configDirectory);
+		
+		const path = require('path');
+			
+		var environment = process.env[environmentName];
+		
+		var config = require(path.join(process.cwd(), configDirectory, environment + '.js'));
+		
+		for (var property in config) {
+			this[property] = config[property];
+			console.log(this[property]);
+		}
+		
+		return this;
 	}
-	console.log('Config environment: ' +  process.env[environmentName]);
-	
-	if(_configDirectory != undefined){
-		configDirectory = _configDirectory;
-	}
-	console.log('Config configDirectory: ' + configDirectory);
-	
-	var environment = process.env[environmentName];
-	
-	var config = require(path.join(process.cwd(), configDirectory, environment + '.js'));
-	
-	return config;
 }
